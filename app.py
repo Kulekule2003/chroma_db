e7094586b78ff689540e015cdefb7977d0269730
kulekule2003 <okureenock@gmail.com>
2026-02-01 22:39:04 +0300

    tase

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ Question: {question}
 # LLM
 # ──────────────────────────
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash",
     google_api_key=GOOGLE_API_KEY
 )
 
